c9c71e701c0ba54a3edcd1c25ff24f1a85f92881
Bhuvana Gayathri  Kanakala <kbgayathri0915@gmail.com>
2023-08-30 17:43:50 +0000

    chat.py

--- chat.py
+++ chat.py
@@ -12,7 +12,10 @@ openai.api_key = api_key
 context = [ {'role':'system', 'content':"""
 you are bot designed to providing assistance and support in multiple areas, \
 such as domestic violence reporting, mental health counseling, career guidance,\
- and emergency contacts. 
+and emergency contacts.your first task or response is to display all the topics in which you can give guidance \
+and then you  ask for the topic in which they want guidance for \
+you respond accordingly to the user queries.\
+you respond in a short and concise way
 
 """} ]  # accumulate messages
 
@@ -31,3 +34,24 @@ def collect_messages_text(msg):
     response = get_completion_from_messages(context) 
     context.append({'role':'assistant', 'content':f"{response}"})
     return response
+
+def send_whatsapp_message(body):
+    # Twilio credentials
+    twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+    twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+    twilio_whatsapp_number = os.getenv("TWILIO_WHATSAPP_NUMBER") # Twilio's sandbox WhatsApp number
+
+    client = Client(twilio_account_sid, twilio_auth_token)
+    
+    
+    try:
+        message = client.messages.create(
+                from_=twilio_whatsapp_number,
+                to='whatsapp:'+i,
+                body="Your order has been placed successfully.\n\n"+result+"\n\nThank you and Visit Again!!!"
+            )
+
+        print('WhatsApp message sent successfully.')
+        print(message.sid)
+    except Exception as e:
+        print('Error sending WhatsApp message:', str(e))
